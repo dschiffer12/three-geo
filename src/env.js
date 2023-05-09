6aadf3928fe9c9cec1b7a7df827dc761a8da6921
Dustin Schiffer <73139931+dschiffer12@users.noreply.github.com>
2023-05-09 14:17:15 +0000

    Create env.js

---
+++ src/env.js
@@ -0,0 +1,6 @@
+export default {
+  zoom: 13, // satellite zoom resolution [11-17]
+  enableTilesLeaflet: true,
+  tokenMapbox:
+    "pk.eyJ1IjoiZHNjaGlmZmVyMTIiLCJhIjoiY2xiZHQ5bTFuMDRtajNvbzQ3c3VudmhvMiJ9.2uvgoSuAn4BT_WJxTGQCZw", // <---- set your Mapbox API token here
+};
